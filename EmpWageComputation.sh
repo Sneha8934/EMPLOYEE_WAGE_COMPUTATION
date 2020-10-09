5c91cf5cdd4185ce9346d277ddee62036ee1abd5
Sneha8934 <snehamane004@gmail.com>
2020-10-09 21:48:16 +0530

    UC1 Committed

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,3 +2,19 @@
 
 echo "------Welcome To Employee Wage Computation------"
 
+function attendance ()
+{
+	Random=$((RANDOM%2))
+	if [ $Random -eq 1 ]
+	then
+		echo "Employee is Present"
+	else
+		echo "Employee is Absent"
+	fi
+}
+
+function main()
+{
+attendance
+}
+main
