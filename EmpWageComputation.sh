67de30378831382f8244460c07ab29467e9f94c2
Sneha8934 <snehamane004@gmail.com>
2020-10-09 21:52:59 +0530

    UC2 Committed

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -18,3 +18,14 @@ function main()
 attendance
 }
 main
+
+isFullTime=1;
+empRatePerHr=20;
+randomCheck=$((RANDOM%2))
+if [ $isFullTime -eq $randomCheck ];
+then
+	empHrs=8;
+else
+	empHrs=0;
+fi
+dailywage=$(($empHrs*$empRatePerHr))
